a4a4d1d6d48ac09fa69cb94f86be5cb672dae512
vvo <vincent.voyer@algolia.com>
2018-01-17 17:58:06 +0100

    fix react example

--- src/React InstantSearch/src/App.js
+++ src/React InstantSearch/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <InstantSearch
           appId="latency"
           apiKey="6be0576ff61c053d5f9a3225e2a90f76"
-          indexName="insant_search"
+          indexName="instant_search"
           searchParameters={{
             hitsPerPage: 8
           }}
@@ -32,7 +32,7 @@ class App extends Component {
 }
 
 function Hit(props) {
-  return <div><Highlight attributeName="title" hit={props.hit} /></div>;
+  return <div><Highlight attributeName="name" hit={props.hit} /></div>;
 }
 
 export default App;
