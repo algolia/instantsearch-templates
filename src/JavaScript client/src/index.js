ec2af3e101f33db64695e5014dbcf607381709ef
vvo <vincent.voyer@algolia.com>
2018-01-18 16:57:22 +0100

    feat(clients): add client and helper

--- src/JavaScript client/src/index.js
+++ src/JavaScript client/src/index.js
@@ -1,1 +1,8 @@
 import './index.css';
+
+import algoliasearch from 'algoliasearch';
+const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+const index = client.initIndex('instant_search');
+index.search('music').then(res => {
+  console.log(res);
+});
--- src/JavaScript helper/src/index.js
+++ src/JavaScript helper/src/index.js
@@ -1,1 +1,11 @@
 import './index.css';
+
+import algoliasearch from 'algoliasearch';
+import algoliasearchHelper from 'algoliasearch-helper';
+const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+const helper = algoliasearchHelper(client, 'instant_search');
+helper.on('result', res => {
+  console.log(res);
+});
+helper.setQueryParameter('query', 'music');
+helper.search();
