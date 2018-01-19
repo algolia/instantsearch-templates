5a8dd6e85b324de9e241b493c4e591edbe254826
vvo <vincent.voyer@algolia.com>
2018-01-19 11:51:06 +0100

    feat(eslint): add full eslint and travis
    fixes #3

--- src/InstantSearch.js/src/index.js
+++ src/InstantSearch.js/src/index.js
@@ -2,21 +2,21 @@
 
 import './index.css';
 
-var search = instantsearch({
+const search = instantsearch({
   appId: 'latency',
   apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
   indexName: 'instant_search',
   searchParameters: {
-    hitsPerPage: 8
-  }
+    hitsPerPage: 8,
+  },
 });
 
 search.addWidget(
   instantsearch.widgets.hits({
     container: document.querySelector('#products'),
     templates: {
-      item: '{{{_highlightResult.name.value}}}'
-    }
+      item: '{{{_highlightResult.name.value}}}',
+    },
   })
 );
 
@@ -24,20 +24,20 @@ search.addWidget(
   instantsearch.widgets.searchBox({
     container: document.querySelector('#searchBox'),
     placeholder: 'Search for products',
-    autofocus: false /* Only to avoid live preview taking focus */
+    autofocus: false /* Only to avoid live preview taking focus */,
   })
 );
 
 search.addWidget(
   instantsearch.widgets.refinementList({
     container: document.querySelector('#brand'),
-    attributeName: 'brand'
+    attributeName: 'brand',
   })
 );
 
 search.addWidget(
   instantsearch.widgets.pagination({
-    container: document.querySelector('#pagination')
+    container: document.querySelector('#pagination'),
   })
 );
 
--- src/JavaScript client/src/index.js
+++ src/JavaScript client/src/index.js
@@ -4,5 +4,6 @@ import algoliasearch from 'algoliasearch';
 const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
 const index = client.initIndex('instant_search');
 index.search('music').then(res => {
+  // eslint-disable-next-line no-console
   console.log(res);
 });
