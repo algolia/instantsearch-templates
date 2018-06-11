9024a3fe2e95cd32f00a1bbdecb6356299590202
vvo <vincent.voyer@algolia.com>
2018-06-11 14:57:00 +0200

    feat(angular-instantsearch): move to v2 of angular-instantsearch
    changes:
    - ng-ais => ais
    - angular cli does not supports well monorepos, using nohoist

--- src/Angular InstantSearch/e2e/protractor.conf.js
+++ src/Angular InstantSearch/e2e/protractor.conf.js
@@ -1,17 +1,15 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-/* global jasmine */
-
-// eslint-disable-next-line import/no-commonjs
 const { SpecReporter } = require('jasmine-spec-reporter');
 
-// eslint-disable-next-line import/no-commonjs
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: ['./e2e/**/*.e2e-spec.ts'],
+  specs: [
+    './src/**/*.e2e-spec.ts'
+  ],
   capabilities: {
-    browserName: 'chrome',
+    'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -19,14 +17,12 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print() {},
+    print: function() {}
   },
   onPrepare() {
     require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json',
+      project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine
-      .getEnv()
-      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  },
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
 };
